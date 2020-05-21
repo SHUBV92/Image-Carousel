2a33ed300648d056c7025b23803c8f7633f91ee5
your Shubinder Virk <shubinder92@gmail.com>
2020-05-21 15:01:42 +0100

    Adding the pictures to the Carosoule

--- carousel/src/Components/Slider/Slider.js
+++ carousel/src/Components/Slider/Slider.js
@@ -1,24 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Slider.scss";
+import ImgComp from "./ImgComp"
+import i1 from "./pics/1.jpg"
 
 const Slider = () => {
-  let sliderArr = [1, 2, 3, 4];
-const [x, setX] = useState(0)
-  const goLeft = () => {
+  let sliderArr = [<ImgComp src={i1}, 2, 3, 4, 5];
+  const [x, setX] = useState(0);
 
-setX(x+100)
+  const goLeft = () => {
+    console.log(x);
+
+    x === 0
+      ? setX(-100 * (sliderArr.length - 1))
+      : setX(x + 100);
+    // if(x<=100){
+    // setX(x + 100)
   };
 
   const goRight = () => {
-setX(x-100)
-      
-
+    console.log(x);
+    x === -100 * (sliderArr.length - 1)
+      ? setX(0)
+      : setX(x - 100);
   };
+
   return (
     <div className="slider">
       {sliderArr.map((item, index) => {
         return (
-          <div key={index} className="slide" style={{transform:`translateX(${x}%)`}}>
+          <div
+            key={index}
+            className="slide"
+            style={{
+              transform: `translateX(${x}%)`
+            }}
+          >
             <h1>{item}</h1>
           </div>
         );
---
+++ carousel/src/ImgComp.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ImgComp = () =>{
+    return<img src={src} alt
+}
